6625ae5b86e23daf895dc71f86251e07e37f7df8
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-09-21 23:29:55 -0300

    Create Quest3.c

---
+++ Exercícios/Lista2/Quest3.c
@@ -0,0 +1,16 @@
+#include <util/delay.h>
+#include<stdio.h>
+
+int main(void)
+{
+    DDRB &= ~((1 << PB0) + (1 << PB1) + (1 << PB2) + ( 1 << PB3) + (1 << PB4)); // Entradas
+    DDRD |= (1 << PD0) ; //SAÍDAS
+    DDRC |= (1 << PC0);
+
+    PORTB = 0b00011111;  //Ativo pull up(Enquanto eu não pressionar eles estão em 1)
+
+    
+    
+
+
+}
